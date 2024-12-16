0b21292b92e8f9b65fa5a86411873b8ca8924099
Blrxamin <bblrx77@gmail.com>
2024-12-16 23:27:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,109 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/hp_lvl', methods=['GET'])
-def get_player_info():
-    return jsonify({"message": "hp نيك امك"})
+@app.route('/check_banned', methods=['GET'])
+def check_banned():
+    player_id = request.args.get('id')
+    
+    if not player_id:
+        return jsonify({'error': 'Player ID is required'}), 400
+    
+    player_info = fetch_player_info(player_id)
+    ban_status = check_ban_status(player_id)
+    
+    if player_info and ban_status is not None:
+        response = {
+            'player_name': player_info['name'],
+            'player_region': player_info['region'],
+            'player_id': player_id,
+            'account_status': 'BANNED' if ban_status else 'NOT BANNED'
+        }
+        return jsonify(response), 200
+    else:
+        return jsonify({'error': 'Could not find information for this player ID or an error occurred'}), 404
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+
+def fetch_player_info(player_id):
+    cookies = {
+        '_ga': 'GA1.1.2123120599.1674510784',
+        '_fbp': 'fb.1.1674510785537.363500115',
+        '_ga_7JZFJ14B0B': 'GS1.1.1674510784.1.1.1674510789.0.0.0',
+        'source': 'mb',
+        'region': 'MA',
+        'language': 'ar',
+        '_ga_TVZ1LG7BEB': 'GS1.1.1674930050.3.1.1674930171.0.0.0',
+        'datadome': '6h5F5cx_GpbuNtAkftMpDjsbLcL3op_5W5Z-npxeT_qcEe_7pvil2EuJ6l~JlYDxEALeyvKTz3~LyC1opQgdP~7~UDJ0jYcP5p20IQlT3aBEIKDYLH~cqdfXnnR6FAL0',
+        'session_key': 'efwfzwesi9ui8drux4pmqix4cosane0y',
+    }
+
+    headers = {
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Origin': 'https://shop2game.com',
+        'Referer': 'https://shop2game.com/app/100067/idlogin',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36',
+        'accept': 'application/json',
+        'content-type': 'application/json',
+        'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'x-datadome-clientid': '6h5F5cx_GpbuNtAkftMpDjsbLcL3op_5W5Z-npxeT_qcEe_7pvil2EuJ6l~JlYDxEALeyvKTz3~LyC1opQgdP~7~UDJ0jYcP5p20IQlT3aBEIKDYLH~cqdfXnnR6FAL0',
+    }
+
+    json_data = {
+        'app_id': 100067,
+        'login_id': f'{player_id}',
+        'app_server_id': 0,
+    }
+
+    res = requests.post('https://shop2game.com/api/auth/player_id_login', cookies=cookies, headers=headers, json=json_data)
+
+    if res.status_code == 200:
+        response = res.json()
+        if 'nickname' in response:
+            name = response['nickname']
+        if 'region' in response:
+            region = response['region']
+
+        return {'name': name, 'region': region}
+    else:
+        return None
+
+
+def check_ban_status(player_id):
+    url = f"https://ff.garena.com/api/antihack/check_banned?lang=en&uid={player_id}"
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+        'Accept': "application/json, text/plain, */*",
+        'authority': "ff.garena.com",
+        'accept-language': "en-GB,en-US;q=0.9,en;q=0.8",
+        'referer': "https://ff.garena.com/en/support/",
+        'sec-ch-ua': "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\"",
+        'sec-ch-ua-mobile': "?1",
+        'sec-ch-ua-platform': "\"Android\"",
+        'sec-fetch-dest': "empty",
+        'sec-fetch-mode': "cors",
+        'sec-fetch-site': "same-origin",
+        'x-requested-with': "B6FksShzIgjfrYImLpTsadjS86sddhFH",
+        'Cookie': "_ga_8RFDT0P8N9=GS1.1.1706295767.2.0.1706295767.0.0.0; apple_state_key=8236785ac31b11ee960a621594e13693; datadome=bbC6XTzUAS0pXgvEs7u",
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            result = response.json()
+            is_banned = result.get('data', {}).get('is_banned', 0)
+            return is_banned == 1
+        else:
+            return None
+    except Exception as e:
+        return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
