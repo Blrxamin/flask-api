cedd8f417dc859699c3ee8a92968a7220f190f4e
Blrxamin <bblrx77@gmail.com>
2024-12-16 18:09:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-.
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/hp_lvl', methods=['GET'])
+def get_player_info():
+    return jsonify({"message": "hp نيك امك"})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
